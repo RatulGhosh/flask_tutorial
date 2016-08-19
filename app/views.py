19f2d080ebc4e6af56507f0c62a3afaa21d1a1d7
RatulGhosh <ratulghoshr@gmail.com>
2016-08-19 23:38:11 +0530

    added for loop

--- app/views.py
+++ app/views.py
@@ -6,6 +6,17 @@ from app import app
 def index():
 	#return "Hello, World!"
     user = {'nickname': 'Ratul'}  # fake user
-    return render_template('index.html',
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template("index.html",
                            title='Home',
-                           user=user)
+                           user=user,
+                           posts=posts)
